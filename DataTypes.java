b191fa252820a01dc9e513614ef2db5ed97e7a23
Shubham-gondaliya <109596173+Shubham-gondaliya@users.noreply.github.com>
2022-07-22 18:10:03 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,24 @@
+class DataTypes
+{
+	public static void main(String[] args) {
+		int num1=15;
+		short myshort=500;
+		byte mybyte = 40;
+		long mylong = 55555555L;
+		float myfloat = 12.20F;
+		double mydouble = 20.55445124;
+		char mychar = 'a';
+		boolean myboolean = false;
+		System.out.println("integer number is :- "+num1);
+		System.out.println("short integer number is :-"+myshort);
+		System.out.println("byte integer number is :-"+mybyte);
+		System.out.println("long integer number is :-"+mylong);
+		System.out.println("float integer number is :-"+myfloat);
+		System.out.println("double integer number is :-"+mydouble);
+		System.out.println("char variable is :- "+mychar);
+		if(myboolean==false)
+		{
+		System.out.println("boolean is :-"+myboolean);
+		}
+	}
+}
