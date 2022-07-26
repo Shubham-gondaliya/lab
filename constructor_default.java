13c5033ff377b2979aa5739c358674a7ce1200c0
Shubham-gondaliya <109596173+Shubham-gondaliya@users.noreply.github.com>
2022-07-26 18:32:37 +0530

    Add files via upload

---
+++ constructor_default.java
@@ -0,0 +1,11 @@
+class constructor_default
+{
+	constructor_default()
+	{
+		System.out.println("this is a default");
+	}
+	public static void main(String[] args) {
+		
+		constructor_default c = new constructor_default();
+	}
+}
