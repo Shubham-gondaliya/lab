47bdb64c80f0a0b396c47073edcc5583936ac3df
Shubham-gondaliya <109596173+Shubham-gondaliya@users.noreply.github.com>
2022-07-27 11:15:45 +0530

    Add files via upload

---
+++ this_keyword.java
@@ -0,0 +1,23 @@
+class demo
+{
+	int id;
+	String name;
+    void demo(int id,String name)
+    {
+    	this.id = id;
+    	this.name = name;
+    }	
+    void show()
+    {
+    	System.out.println(id+ " " +name);
+    }
+
+}
+class this_keyword
+{
+	public static void main(String[] args) {
+		demo d = new demo();
+		d.demo(10,"shubham");
+		d.show();
+	}
+}
